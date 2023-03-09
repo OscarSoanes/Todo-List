712e359694517e79456b5cef6d119e6a60161ea6
Oscar Soanes <oscarsoanes@gmail.com>
2023-03-09 13:55:30 +0000

    projects of project are now created

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import './reset.css';
 import Project from './modules/project.js';
 import Todo from './modules/todo.js';
 
-const element = document.createElement('p');
-element.textContent = "Hi"
-element.classList.add("Hello");
-
-document.body.append(element);
+let projects = [];
 
 let ProjectTest = new Project("test");
+projects.push(ProjectTest);
 let newTodo = new Todo("test", "testDesc", "10-12-13", "high");
 
 ProjectTest.newTodo(newTodo);
+ProjectTest.newTodo(newTodo);
+ProjectTest.newTodo(newTodo);
 
+let NextProject = new Project("more testing");
+NextProject.newTodo(newTodo);
+NextProject.newTodo(newTodo);
+
+projects.push(NextProject);
 console.log(ProjectTest);
+console.log(projects);
